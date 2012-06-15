f04a5e6d28abc81eac2172d7fbf3a0f781b9388a
David Cotrim <davidmirtoc@hotmail.com>
2012-06-15 15:02:46 +0100

    commit to tag

---
+++ src/org/example/viewer/beans/LibPublisherEditBean.java
@@ -0,0 +1,13 @@
+package org.example.viewer.beans;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import netgest.bo.xwc.xeo.beans.XEOEditBean;
+
+
+public class LibPublisherEditBean extends XEOEditBean {
+	
+	//public DataListConnector getDataList(){ }
+		
+
+}
